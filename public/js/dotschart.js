cd727e83f520d237a45a1549530821f33f366fc6
ngeorgiev <niki@maj.io>
2015-04-21 20:01:34 +0300

    Created cool dots graphic

--- public/js/dotschart.js
+++ public/js/dotschart.js
@@ -1,4 +1,86 @@
-$(function () {
+var width = 700,
+    height = 350,
+    leftgutter = 10,
+    topgutter = 10,
+    strongBias = 0.8,
+    weakBias = 0.4,
+    xAxisSize = 100,
+    yAxisSize = 50;
+
+var radiusMap = {
+    0: 1,
+    10: 4,
+    20: 5,
+    30: 6,
+    40: 7,
+    50: 15,
+    60: 17,
+    70: 20,
+    80: 21,
+    90: 24,
+    100: 37
+}
+var maxRadius = radiusMap[100],
+    maxWidth = width - maxRadius,
+    minWidth = leftgutter + maxRadius,
+    maxHeight = height - maxRadius,
+    minHeight = topgutter + maxRadius,
+    xScaleSize = getScaleSize(0, maxWidth, xAxisSize),
+    yScaleSize = getScaleSize(0, maxHeight, yAxisSize);
+
+function dotschart(cultureFit, containerId) {
+    var data = [];
+    var paper = Raphael(containerId, width, height);
+
+    $.each(cultureFit.all, function(idx, fit) {
+        drawCultureFitCircle(paper, fit);
+    });
+
+    var currProfile = drawCultureFitCircle(paper, cultureFit.current);
+    currProfile.node.setAttribute('class', 'current-profile-fit');
+    var x = currProfile.attr('cx');
+    var y = currProfile.attr('cy');
+    var radius = currProfile.attr('r');
+
+    paper.circle(x, y, radius + 6).node.setAttribute('class', 'current-profile-fit-border');
+    paper.text(x, y, cultureFit.current.name).attr({"font": '12px Verdana, Fontin-Sans, Arial', 'font-weight': 'bold', stroke: "none", fill: "#fff"});
+}
+
+function drawCultureFitCircle(paper, fit) {
+    var normFitVal = fit.value * xScaleSize;
+    var randomY = randomIntBetween(minHeight, maxHeight);
+    var circle = paper.circle(normFitVal, randomY, getRadius(fit.value));
+    // paper.circle(normFitVal, randomY, 2).attr({fill: '#f00', stroke: 'none'});
+
+    if (normFitVal > (maxWidth * strongBias)) {
+        circle.node.setAttribute('class', 'strong-fit');
+    } else if (normFitVal < (maxWidth * weakBias)) {
+        circle.node.setAttribute('class', 'weak-fit');
+    } else {
+        // the gradient colors must match the ones for strong-fit and weak-fit styles
+        circle.attr({fill: '0-#f16460-#b95d9f', 'fill-opacity': 0.7, opacity: 0.7, stroke: 'none'});
+    }
+    return circle;
+}
+
+function getScaleSize(min, max, size) {
+    return (max - min) / size;
+}
+
+function norm(min, max, val) {
+    return (val / max) * 100;
+}
+
+function randomIntBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRadius(fitValue) {
+    var floor = Math.floor(fitValue / 10) * 10;
+    return radiusMap[floor];
+}
+
+function oldDotchart() {
     // Grab the data
     // var data = [],
     //     axisx = [],
@@ -48,7 +130,7 @@ $(function () {
             o++;
         }
     }
-});
+}
 
 
 function drawCircle(dx, dy, R, value, r, o) {
--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,59 @@
 $(document).ready(function() {
-	spiderchart(data, 'spiderchart-container');
-	sliderchart(data, 'sliderchart-container');
+	var data = initData();
+
+	spiderchart(data.data, 'spiderchart-container');
+	sliderchart(data.data, 'sliderchart-container');
+	dotschart(data.cultureFit, 'dotschart-container');
 });
+
+function randomName() {
+  return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+}
+
+function randomValue() {
+  return parseInt(((Math.random() * 100) + 1).toFixed(2), 10);
+}
+
+function initData() {
+	var cultureFitValues = [];
+
+	for (var i = 0; i < 100; i++) {
+	  cultureFitValues.push({
+	    name: randomName(),
+	    value: randomValue()
+	  });
+	}
+	return {
+		data: [
+		  {
+		    className: 'company-values',
+		    axes: [
+		      {axis: "Exceed client expectation", value: 100},
+		      {axis: "Communicate openly", value: 89},
+		      {axis: "Innovate", value: 54},
+		      {axis: "Embrace diversity", value: 92},
+		      {axis: "Lead the way", value: 77},
+		      {axis: "Constantly learn", value: 34}
+		    ]
+		  },
+		  {
+		    className: "person-values",
+		    axes: [
+		      {axis: "Exceed client\n expectation", value: 88},
+		      {axis: "Communicate openly", value: 68},
+		      {axis: "Innovate", value: 78},
+		      {axis: "Embrace diversity", value: 90},
+		      {axis: "Lead the way", value: 40},
+		      {axis: "Constantly learn", value: 57}
+		    ]
+		  }
+		],
+		cultureFit: {
+			all: cultureFitValues,
+			current: {
+				name: randomName(),
+				value: 94
+			}
+		}
+	};
+}
